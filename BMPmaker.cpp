b43247d5f1a557af37b6a7b1cf6e35d0b21f7cb2
Johan Lindell <lindell92@gmail.com>
2012-05-05 18:28:28 +0200

    First woking version

--- BMPmaker.cpp
+++ BMPmaker.cpp
@@ -1,37 +1,115 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 using namespace std;
 
+
+
+void printMultipleBits(char file[],int data,int &startPos,int bits)
+{
+	int devide;
+	for(int i=0;i<bits;i++){
+		if(i==0){
+			devide = 1;
+		}
+		else{
+			devide = 256*i;
+		}
+		file[startPos + i] = (data/devide) % 256;
+	}
+	startPos += bits;
+}
+
+void printLongInt(char file[],int longint,int &startPos)
+{
+	printMultipleBits(file,longint,startPos,4);
+}
+
+void printWord(char file[],int word,int &startPos)
+{
+	printMultipleBits(file,word,startPos,2);
+}
+
+void printByte(char file[],int byte,int &startPos)
+{
+	printMultipleBits(file,byte,startPos,1);
+}
+
 int main () {
-	ofstream myfile;
-	myfile.open ("test.bmp");
+	
+	//Count the number of characters and put the characters in a string
+	char c;
+	string data;
+	int count = 0;
+    cin >> noskipws;    // Stops all further whitespace skipping
+    while (cin >> c) {  // Reads whitespace chars now.
+		data += c;
+		count++;
+    }
+	
 	
 	//Standard
-	int headerSize = 0x35;
+	int headerSize = 0x36;
 	
 	
 	//More specifik settings
-	int imageWidth = 4;
-	int imageHeight = 12;
+	int pixelsNeeded = ceil(count/3.0);
 	
-	int fileSize = headerSize+imageWidth*imageHeight*3;
+	int imageWidth;
+	if(pixelsNeeded/4 % 4 == 0){
+		imageWidth = pixelsNeeded/4;
+	}
+	else{
+		imageWidth = pixelsNeeded/4+4-(pixelsNeeded/4)%4;
+	}
+	
+	int imageHeight = 4;
+	
+	int imageDataSize = imageWidth*imageHeight*3;
+	
+	int fileSize = headerSize+imageDataSize;
+	
+	// typedef char fileBuffer[fileSize];
 	
 	//The data to the file is stored in this variable
 	char bmpfile[fileSize];
-
+	
+	
+	int i = 0;
 	//
 	//  Header
 	//
-	bmpfile[0] = 0x42; //B
-	bmpfile[1] = 0x4D; //M
-	bmpfile[2] = 0x00; //Size of image
-	bmpfile[3] = 0x00;
-	bmpfile[4] = 0x00;
-	bmpfile[5] = 0x00;
-
+	bmpfile[i++] = 0x42; //B
+	bmpfile[i++] = 0x4D; //M
+	printLongInt(bmpfile,fileSize,i); //Filesize
+	printLongInt(bmpfile,0,i); //Reserved
+	printLongInt(bmpfile,0x36,i); //Headersize
+	printLongInt(bmpfile,0x28,i); 
+	printLongInt(bmpfile,imageWidth,i);
+	printLongInt(bmpfile,imageHeight,i);
+	printWord(bmpfile,0x01,i);
+	printWord(bmpfile,0x18,i);
+	printLongInt(bmpfile,0x00,i);
+	printLongInt(bmpfile,imageDataSize,i);
+	printLongInt(bmpfile,0,i);
+	printLongInt(bmpfile,0,i);
+	printLongInt(bmpfile,0,i);
+	printLongInt(bmpfile,0,i);
+	
+	//Put the data into the image
+	for(int a=0;a<count;a++){
+		printByte(bmpfile,data[a],i);
+	}
+	
+	
+	//Write to file
+	ofstream myfile;
+	myfile.open ("test.bmp");
 	myfile.write(bmpfile, fileSize);
 	// myfile << bmpfile;
 
 	myfile.close();
 	return 0;
 }
+
+
